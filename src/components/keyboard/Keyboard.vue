438ce663310489c230aefcce6c61f4de7caf9594
Gabriel Laroche <gabriel@centiva.ca>
2022-02-16 13:52:04 -0500

    First commit

---
+++ src/components/keyboard/Keyboard.vue
@@ -0,0 +1,28 @@
+<template>
+<ul>
+	<li
+		v-for="letter in letters"
+		:key="letter"
+	>
+		<button>
+			{{ letter }}
+		</button>
+	</li>
+</ul>
+</template>
+
+<script>
+export default {
+	name: 'Keyboard',
+
+	data() {
+		return {
+			letters: ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/word-grid/WordGrid.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		grid
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'WordGrid'
+}
+</script>
+
+<style scoped>
+
+</style>
